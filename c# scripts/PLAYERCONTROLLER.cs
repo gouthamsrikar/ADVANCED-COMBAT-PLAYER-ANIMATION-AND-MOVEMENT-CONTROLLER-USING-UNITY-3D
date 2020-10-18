fdab3cb042abc98e0d53c0bb5d01dfc695c37813
gouthamsrikar <53181084+gouthamsrikar@users.noreply.github.com>
2020-10-18 20:26:48 +0530

    Update PLAYERCONTROLLER.cs
    some animation blend errors fixed and movement added

--- c# scripts/PLAYERCONTROLLER.cs
+++ c# scripts/PLAYERCONTROLLER.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PLAYERCONTROLLER : MonoBehaviour
 {
     public Animator anim;
+    public GameObject camera;
 
     public bool weapon=false;
     public float forward=0;
@@ -15,7 +16,13 @@ public class PLAYERCONTROLLER : MonoBehaviour
     public float turn;
     public bool turnbool;
 
-    public float buttonsensitivity=1;
+    public float buttonsensitivity=1f;
+    public float mousesensitivity = 100f;
+    public float moveSpeed = 5f;
+    public float axisX;
+    public float axisY;
+
+
 
     void Start()
     {
@@ -70,11 +77,13 @@ public class PLAYERCONTROLLER : MonoBehaviour
                 forward -= Time.deltaTime * buttonsensitivity;
             }
             anim.SetFloat("forward", forward);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime*2, Space.Self);
         }
         else if(forward>0)
         {
             forward -= Time.deltaTime * buttonsensitivity;
             anim.SetFloat("forward", forward);
+          //  transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime *2, Space.Self);
         }
 
         if(Input.GetKey(KeyCode.DownArrow) && forward>-2)           //to walk backward
@@ -92,11 +101,13 @@ public class PLAYERCONTROLLER : MonoBehaviour
                 forward += Time.deltaTime * buttonsensitivity;
             }
             anim.SetFloat("forward", forward);
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime , Space.Self);
         }
         else if(forward<0)
         {
             forward += Time.deltaTime * buttonsensitivity;
             anim.SetFloat("forward", forward);
+         //   transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime , Space.Self);
         }
 
         if (Input.GetKey(KeyCode.RightArrow) && backward < 2)           //to walk right
@@ -115,11 +126,13 @@ public class PLAYERCONTROLLER : MonoBehaviour
                 backward -= Time.deltaTime * buttonsensitivity;
             }
             anim.SetFloat("backward", backward);
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.Self);
         }
         else if(backward>0)
         {
             backward -= Time.deltaTime * buttonsensitivity;
             anim.SetFloat("backward", backward);
+        //   transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.Self);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) && backward>-2)         //to walk left
@@ -137,13 +150,40 @@ public class PLAYERCONTROLLER : MonoBehaviour
                 backward += Time.deltaTime * buttonsensitivity;
             }
             anim.SetFloat("backward", backward);
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime , Space.Self);
         }
         else if (backward < 0)
         {
             backward += Time.deltaTime * buttonsensitivity;
             anim.SetFloat("backward", backward);
+         //   transform.Translate(Vector3.right * moveSpeed * Time.deltaTime , Space.Self);
         }
 
+        // turn around using mouse x axis
+        axisX = Input.GetAxis("Mouse X");
+        transform.RotateAround(transform.position, Vector3.up, axisX* mousesensitivity * Time.deltaTime);
+        if (Input.GetAxis("Mouse X")>0f )
+        {
+            turn =100*Time.deltaTime;
+            anim.SetFloat("TURN", turn);          
+        }
+        else if(Input.GetAxis("Mouse X") < 0f )
+        {
+            turn=-100*Time.deltaTime;
+            anim.SetFloat("TURN", turn);
+          //  transform.RotateAround(transform.position, Vector3.up, -axis * mousesensitivity * Time.deltaTime);
+        }
+        if(axisX==0)
+        {
+            turn = 0;
+            anim.SetFloat("TURN", turn);
+        }
 
+        //camera vertical movement
+        axisY = Input.GetAxis("Mouse Y");
+       //  camera.transform.RotateAroundLocal( Vector3.right, axisY * mousesensitivity * Time.deltaTime);
+
+        
+      
     } //end update
 }
